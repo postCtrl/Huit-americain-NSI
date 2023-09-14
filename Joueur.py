064e8dc364bea7ea972b1ee47085e2496a5ae70e
postCtrl <jourdainmichel55@gmail.com>
2023-09-14 23:08:09 +0200

    Tirer carte à finir

--- Joueur.py
+++ Joueur.py
@@ -13,13 +13,17 @@ class Joueur:
 		for i in self.paquet:
 			index += 1
 			print(f"{index}. {self.nom} a le {i.valeur} de {i.couleur}")
+		print("")
 
 	def poserCartes(self, defausse):
-		r = int(input("Pour choisir votre carte sélectionnez l'indice de la carte que vous voulez jouer: ")) - 1
+		r = input("Pour choisir votre carte sélectionnez l'indice de la carte que vous voulez jouer: ") - 1
 
-		if self.paquet[r].couleur == defausse.couleur or self.paquet[r] == defausse.valeur:
-			return self.paquet[r]
+		while self.paquet[r].couleur != defausse.couleur or self.paquet[r] != defausse.valeur:
+			r = input("La carte choisie n'est peut pas être posée, choisissez en une autre ou tirez une carte avec 't': ")
+			
+			if r.lower() == 't':
+				return False 
 
-		return False
+			r = int(r) 
 
-
+		return self.paquet[r]
--- Main.py
+++ Main.py
@@ -8,8 +8,10 @@ class Main:
 		self.pile = [Carte(self.couleurs_cartes[i], j) for i in range(4) for j in range(1, 14)]
 		self.defausse = [] #là où les joueurs posent leurs cartes
 		self.joueurs = [] #liste d'objets contenant les joueurs du jeu
+		
 		r=""
 		i = 0
+
 		while len(self.joueurs) < 7 and r.lower() != "s":
 			i+=1
 			r = input(f"Entrez le nom du joueur {i}, si vous avez suffisament de joueurs tapez 's': ")
@@ -19,6 +21,8 @@ class Main:
 
 			self.joueurs.append(Joueur(r))
 
+		print("\n")
+
 	def distribuerCartes(self):
 		for _ in range(7):
 			for i in self.joueurs:
@@ -27,15 +31,17 @@ class Main:
 		self.defausse.append(self.pile.pop())
 
 	def phaseDeJeu(self):
+		print(f"Voici la défausse: {self.defausse[-1].valeur} de {self.defausse[-1].couleur}\n")
+
+		r = False
+
 		for i in self.joueurs:
 			i.voirCartes()
 
-			r = i.poserCartes(self.defausse[-1])
+			while r == False:
+				r = i.poserCartes(self.defausse[-1])
 
-			if r == False :
-				i.ajoutCarte(self.pile.pop())
-
-
+			i.ajoutCarte(self.pile.pop())
 
 
 
