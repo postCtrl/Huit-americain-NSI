2685da3956ea40b9de67e6a83b3cd8ecd502820f
Abraham <jourdainmichel55@gmail.com>
2023-09-15 11:02:10 +0200

    Erreur tirage corrigée

--- Joueur.py
+++ Joueur.py
@@ -18,7 +18,11 @@ class Joueur:
 		print("")
 
 	def poserCartes(self, defausse):
-		r = int(input("Pour choisir votre carte sélectionnez l'indice de la carte que vous voulez jouer: ")) - 1
+		r = int(input("Pour choisir votre carte sélectionnez l'indice de la carte que vous voulez jouer ou tirez une carte avec 't': ")) - 1
+
+		if r.lower() == 't':
+			return False
+
 
 		while self.main[r].compatible(defausse) == False: #tant que le joueur n'a pas joué de carte compatible
 			r = input("La carte choisie n'est peut pas être posée, choisissez en une autre ou tirez une carte avec 't': ") #on demande au joueur si il a encore une carte compatible
@@ -26,7 +30,7 @@ class Joueur:
 			if r.lower() == 't':#si le joueur préfère tirer une carte
 				return False 
 
-			r = int(r) #ne pas avoir d'erreur lorsque j'uilise 'r' comme indice
+			r = int(r) -1 #ne pas avoir d'erreur lorsque j'uilise 'r' comme indice
 
 		e = self.main.pop(r) # on retire la carte utilisée	
 		return e # on la retourne également pour la placer dans le défausse
